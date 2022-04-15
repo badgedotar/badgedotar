ec6a218ff862de71324d95b8af1d79d9faa5872a
Cavallo Victor Gabriel <gabriel.cavallo@coderhouse.com>
2022-04-15 19:52:01 -0300

    add appwrite deployment

---
+++ scripts/deploy-backend-server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Get server list
+set -f
+string=$DEPLOY_IP	
+array=(${string//,/ })
+
+for i in "${!array[@]}"; do
+  echo "Deploying backend server on ${array[$i]}"
+  ssh -i ~/.ssh/id_rsa ubuntu@${array[i]} "export _APP_OPENSSL_KEY_V1=$_APP_OPENSSL_KEY_V1 && export _APP_LOGGING_CONFIG=$_APP_LOGGING_CONFIG && export _APP_DB_PASS=$_APP_DB_PASS && export _APP_DB_ROOT_PASS=$_APP_DB_ROOT_PASS && export _APP_EXECUTOR_SECRET=$_APP_EXECUTOR_SECRET &&export DOCKERHUB_PULL_PASSWORD=$DOCKERHUB_PULL_PASSWORD && cd /home/ubuntu/badgear/badgedotar && git config --global user.name \"badgear\" && git config --global user.email gitlab@actions && git pull && cd backend_server && docker-compose --env-file ./.env up -d"
+done
