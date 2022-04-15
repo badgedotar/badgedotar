eaa9ac5991c0430dbbf839d325898b989e253205
Cavallo Victor Gabriel <gabriel.cavallo@coderhouse.com>
2022-04-14 22:56:47 -0300

    add certificate path

--- scripts/deploy-nodes.sh
+++ scripts/deploy-nodes.sh
@@ -7,5 +7,5 @@ array=(${string//,/ })
 
 for i in "${!array[@]}"; do
   echo "Deploying cardano node on ${array[$i]}"
-  ssh ubuntu@${array[i]} "cd /home/ubuntu/badgear/badgedotar && git config --global user.name \"badgear\" && git config --global user.email gitlab@actions && git pull && cd cardano_nodes && docker-compose up -d"
+  ssh -i ~/.ssh/id_rsa ubuntu@${array[i]} "cd /home/ubuntu/badgear/badgedotar && git config --global user.name \"badgear\" && git config --global user.email gitlab@actions && git pull && cd cardano_nodes && docker-compose up -d"
 done
