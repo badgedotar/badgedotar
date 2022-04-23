46b1c598916ce5c7feae3308253e585618e35e46
Cavallo Victor Gabriel <gabriel.cavallo@coderhouse.com>
2022-04-22 22:13:57 -0300

    add envfile to startup command

--- scripts/deploy-bridge.sh
+++ scripts/deploy-bridge.sh
@@ -7,5 +7,5 @@ array=(${string//,/ })
 
 for i in "${!array[@]}"; do
   echo "Deploying cardano bridge on ${array[$i]}"
-  ssh -i ~/.ssh/id_rsa ubuntu@${array[i]} "export APPWRITE_KEY=$APPWRITE_KEY && cd /home/ubuntu/badgear/badgedotar && git config --global user.name \"badgear\" && git config --global user.email gitlab@actions && git pull && cd cardano-bridge && docker-compose up -d"
+  ssh -i ~/.ssh/id_rsa ubuntu@${array[i]} "export APPWRITE_KEY=$APPWRITE_KEY && cd /home/ubuntu/badgear/badgedotar && git config --global user.name \"badgear\" && git config --global user.email gitlab@actions && git pull && cd cardano-bridge && docker-compose up --env-file ./.env up -d"
 done
