d4364538998eeb93dc4f4806777b5198ef156042
Cavallo Victor Gabriel <cavallo.v.gabriel@gmail.com>
2022-04-28 00:47:02 -0300

    fix typo

--- scripts/deploy-ipfs.sh
+++ scripts/deploy-ipfs.sh
@@ -7,5 +7,5 @@ array=(${string//,/ })
 
 for i in "${!array[@]}"; do
   echo "Deploying IPFS on ${array[$i]}"
-  ssh -i ~/.ssh/id_rsa ubuntu@${array[i]} "cd /home/ubuntu/badgear/badgedotar && git config --global user.name badgear && git config --global user.email gitlab@actions && git pull && cd ipfs-clister-ctl && docker-compose --env-file ./.env up -d"
+  ssh -i ~/.ssh/id_rsa ubuntu@${array[i]} "cd /home/ubuntu/badgear/badgedotar && git config --global user.name badgear && git config --global user.email gitlab@actions && git pull && cd ipfs-cluster-ctl && docker-compose --env-file ./.env up -d"
 done
