aa3d9bb0ca9d7b9da5e46594b1e37cbeac70b4b9
Catriel Müller <catrielmuller@gmail.com>
2022-05-12 00:14:56 -0300

    Add Favicon

--- frontend/pages/_app.page.tsx
+++ frontend/pages/_app.page.tsx
@@ -25,6 +25,13 @@ export default function MyApp(props: MyAppProps) {
       <CacheProvider value={emotionCache}>
         <Head>
           <meta name="viewport" content="initial-scale=1, width=device-width" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#333333" />
+          <meta name="msapplication-TileColor" content="#333333" />
+          <meta name="theme-color" content="#333333" />
         </Head>
         <ThemeProvider theme={theme}>
           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
